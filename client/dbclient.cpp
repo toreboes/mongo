d4f6de2e889dfa9631c3068009d31388403e8cb8
Tore B. Østensen <tore@ostensen.net>
2011-07-08 01:12:38 +0200

    Dummy edit

--- client/dbclient.cpp
+++ client/dbclient.cpp
@@ -1,7 +1,7 @@
 // dbclient.cpp - connect to a Mongo database as a database, from C++
 
 /*    Copyright 2009 10gen Inc.
- *
+ * dummy edit
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
